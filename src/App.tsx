9826749ffe3450746c3aa6e082443e16b0274522
Anish Duwal <duwalanise@gmail.com>
2019-04-06 11:49:39 +0545

    animated nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,31 @@
 import * as React from "react";
-import { useTransition, animated } from "react-spring";
+import { useTransition } from "react-spring";
 
-import "./App.css";
 import { useState, useCallback } from "react";
+import Page from "./components/page/page";
+import Nav from "./components/nav/nav";
 
 interface AppProps {}
 
-const pages = [
-  ({ style }: { style: any }) => (
-    <animated.div
-      className="page"
-      style={{ ...style, background: "lightpink" }}
-    >
-      Namaste
-    </animated.div>
-  ),
-  ({ style }: { style: any }) => (
-    <animated.div
-      className="page"
-      style={{ ...style, background: "lightblue" }}
-    >
-      Anish
-    </animated.div>
-  ),
-  ({ style }: { style: any }) => (
-    <animated.div
-      className="page"
-      style={{ ...style, background: "lightgreen" }}
-    >
-      Duwal
-    </animated.div>
-  )
-];
+const pages = ["Namaste", "Anish", "Duwal"];
 
 const App: React.FunctionComponent<AppProps> = ({}) => {
-  const [index, set] = useState(0);
-  const onClick = useCallback(() => set(state => (state + 1) % 3), []);
+  const [index, setIndex] = useState(0);
+  const onClick = useCallback(idx => setIndex(idx), []);
   const transitions = useTransition(index, p => p, {
     from: { opacity: 0, transform: "translate3d(100%,0,0)" },
     enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
     leave: { opacity: 0, transform: "translate3d(-50%,0,0)" }
   });
   return (
-    <div className="wrapper">
-      <div className="nav" onClick={onClick}>
-        Menu
-      </div>
+    <div>
+      <Nav pages={pages} onClick={onClick} />
       {transitions.map(({ item, props, key }) => {
-        const Page = pages[item];
-        return <Page key={key} style={props} />;
+        return (
+          <Page key={key} style={props}>
+            {pages[item]}
+          </Page>
+        );
       })}
     </div>
   );
